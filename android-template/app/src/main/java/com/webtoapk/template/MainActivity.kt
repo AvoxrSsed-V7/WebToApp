3e06c8dcc6d6fee64beae121f31dc53703b0e61a
Dyy2 <haddii670@gmail.com>
2026-09-06 00:18:46 +0700

    Add complete Android template structure with Gradle config, Kotlin activity, and resources

---
+++ android-template/app/src/main/java/com/webtoapk/template/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.webtoapk.template
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl(getString(R.string.app_url))
+
+        setContentView(webView)
+    }
+
+    override fun onBackPressed() {
+        // Kalau masih bisa navigasi mundur di dalam WebView, jangan langsung close app
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
